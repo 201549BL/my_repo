8d62f6d0d01d524512e78d5a5a326c046de1294a
201549 <201549@stud.himolde.no>
2020-10-12 19:39:44 +0200

    first git upload

--- web_scrapping.py
+++ web_scrapping.py
@@ -4,6 +4,19 @@ import timeit
 from PIL import Image
 from bs4 import BeautifulSoup
 
+
+def get_soup_image(url):
+    webpage = requests.get(url)
+    soup = bs4.BeautifulSoup(webpage.text, 'html.parser')
+    image = soup.find(id='img src')
+    return image
+
+
+counter = 1
+while True:
+    image = get_soup_image(f'https://www.xkcd.com/{counter}')
+
+
 # Web scrapping program.
 # Write your code here. Have fun!
 
